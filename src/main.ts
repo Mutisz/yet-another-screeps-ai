4f90a8e6864a007dd262a50d918aa275c70d7bef
Mateusz Tokarski <mati.tokarski@gmail.com>
2023-11-29 15:09:59 +0100

    Boilerplate commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+export const loop = () => {
+  console.log(`Current game tick is ${Game.time}`);
+  for (const name in Memory.creeps) {
+    if (!(name in Game.creeps)) {
+      delete Memory.creeps[name];
+    }
+  }
+};
