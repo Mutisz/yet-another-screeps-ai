db93501114137aa944f8d06aac5b82a3c08571e3
Mateusz <mati.tokarski@gmail.com>
2023-12-16 14:58:00 +0100

    Validation of global fn input + small refactor

--- src/const.ts
+++ src/const.ts
@@ -1,17 +1,17 @@
 export type RoomLevel = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 export type CreepRoleConstant = typeof ROLE_BUILDER | typeof ROLE_HARVESTER | typeof ROLE_UPGRADER;
-export type CreepActionConstant = typeof ACTION_UPGRADE | typeof ACTION_WITHDRAW;
+export type CreepActionConstant = typeof ACTION_BUILD | typeof ACTION_UPGRADE | typeof ACTION_WITHDRAW;
+
+export const MAIN_SPAWN_NAME = 'MainSpawn';
 
 export const ROLE_BUILDER = 'builder';
 export const ROLE_HARVESTER = 'harvester';
 export const ROLE_UPGRADER = 'upgrader';
 
+export const ACTION_BUILD = 'build';
 export const ACTION_UPGRADE = 'upgrade';
 export const ACTION_WITHDRAW = 'withdraw';
 
 export const WORKER_ROLE_LIST = [ROLE_BUILDER, ROLE_HARVESTER, ROLE_UPGRADER];
-
-export const MAIN_SPAWN_NAME = 'MainSpawn';
-export const WORKER_RALLY_POINT_NAME = 'WorkerRallyPoint';
-
 export const WORKER_BODY = [CARRY, WORK, MOVE];
+export const WORKER_RALLY_POINT_NAME = 'WorkerRallyPoint';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { isEmpty } from 'lodash';
-import { CreepActionConstant, CreepRoleConstant, MAIN_SPAWN_NAME, RoomLevel } from './config/config';
+import { CreepActionConstant, CreepRoleConstant, MAIN_SPAWN_NAME, RoomLevel } from './const';
 import { onTick as constructionManagerOnTick, planRoad, RoadStatus } from './construction/manager';
 import { onTick as creepManagerOnTick } from './creep/manager';
+import { pruneCreepMemory } from './util/garbageCollector';
 
 type RoomObjectId = Id<_HasId & RoomObject>;
 
@@ -42,14 +43,29 @@ global.setControllerLevel = (roomName: string, controllerLevel: RoomLevel): void
 };
 
 global.setWorkerCountAll = (roomName: string, count: number): void => {
-  Game.rooms[roomName].memory.config.workerCountAll = count;
+  const roomConfig = Game.rooms[roomName].memory.config;
+  if (count <= roomConfig.workerCountBuilder + roomConfig.workerCountUpgrader) {
+    throw new Error('All worker count cannot be lower or equal to builder and upgrader count!');
+  }
+
+  roomConfig.workerCountAll = count;
 };
 
 global.setWorkerCountBuilder = (roomName: string, count: number): void => {
-  Game.rooms[roomName].memory.config.workerCountBuilder = count;
+  const roomConfig = Game.rooms[roomName].memory.config;
+  if (count < 0 || count >= roomConfig.workerCountAll - roomConfig.workerCountUpgrader) {
+    throw new Error('Builder count cannot be negative and at least one worker must be a harvester!');
+  }
+
+  roomConfig.workerCountBuilder = count;
 };
 
 global.setWorkerCountUpgrader = (roomName: string, count: number): void => {
+  const roomConfig = Game.rooms[roomName].memory.config;
+  if (count < 0 || count >= roomConfig.workerCountAll - roomConfig.workerCountBuilder) {
+    throw new Error('Upgrader count cannot be negative and at least one worker must be a harvester!');
+  }
+
   Game.rooms[roomName].memory.config.workerCountUpgrader = count;
 };
 
@@ -65,7 +81,7 @@ global.planRoad = (originId: RoomObjectId, targetId: RoomObjectId): void => {
 
 const createDefaultRoomMemory = (): RoomMemory => ({
   config: {
-    controllerLevel: 2,
+    controllerLevel: 3,
     workerCountAll: 6,
     workerCountBuilder: 2,
     workerCountUpgrader: 2,
@@ -86,9 +102,5 @@ export const loop = () => {
   initializeMemory(spawn.room);
   constructionManagerOnTick(spawn.room);
   creepManagerOnTick(spawn);
-  for (const name in Memory.creeps) {
-    if (!(name in Game.creeps)) {
-      delete Memory.creeps[name];
-    }
-  }
+  pruneCreepMemory();
 };
